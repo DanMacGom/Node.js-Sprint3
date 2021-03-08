1d88e1af9c9eba6251a0b5205e8cc082c1b88621
DanMacGom <daniel.macias.gomez@gmail.com>
2021-03-08 20:15:33 +0100

    Main

--- 2.patterns/Nivell-1/Exercici-1/callback_hell.js
+++ 2.patterns/Nivell-1/Exercici-1/callback_hell.js
@@ -4,53 +4,31 @@ const { join } = require("path");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
+const reverseText = str =>
+  str
+  .split("")
+  .reverse()
+  .join("");
+
 // Read and reverse contents of text files in a directory
-// fs.readdir(inbox, (error, files) => {
-//   if (error) {
-//     return console.log("Error: Folder inaccessible");
-//   }
-//
-//   files.forEach(file => {
-//     fs.readFile(join(inbox, file), "utf8", (error, data) => {
-//       if (error) {
-//         return console.log("Error: File error");
-//       }
-//
-//       fs.writeFile(join(outbox, file), reverseText(data), error => {
-//         if (error) {
-//           return console.log("Error: File could not be saved!");
-//         }
-//
-//         console.log(`${file} was successfully saved in the outbox!`);
-//       });
-//     });
-//   });
-// });
+fs.readdir(inbox, (error, files) => {
+  if (error) {
+    return console.log("Error: Folder inaccessible");
+  }
 
-function getFileNames(dir_path, callback) {
-  fs.readdir(dir_path, function(err, files) {
-    if (err) throw err;
+  files.forEach(file => {
+    fs.readFile(join(inbox, file), "utf8", (error, data) => {
+      if (error) {
+        return console.log("Error: File error");
+      }
 
-    callback(files);
+      fs.writeFile(join(outbox, file), reverseText(data), error => {
+        if (error) {
+          return console.log("Error: File could not be saved!");
+        }
+
+        console.log(`${file} was successfully saved in the outbox!`);
+      });
+    });
   });
-}
-
-function readFiles(file_names, callback) {
-  fs.readFile()
-}
-
-getFileNames(inbox, files => { console.log(files) });
-// function getFileNames(file_name, readdir) {
-//   fs.readdir(
-//     file_name,
-//     function(err, files) {
-//       if (err)
-//         throw err;
-//
-//       // folderName(files);
-//   })
-// };
-
-// console.log(getFileNames(inbox));
-
-// const reverseText = str => str.split("").reverse().join("");
+});
