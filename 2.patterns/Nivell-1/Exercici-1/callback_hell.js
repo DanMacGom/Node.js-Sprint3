cae938d1b7238e52fe353bfbddbb388e25f6d89c
DanMacGom <daniel.macias.gomez@gmail.com>
2021-03-09 10:38:21 +0100

    Commented code

--- 2.patterns/Nivell-1/Exercici-1/callback_hell.js
+++ 2.patterns/Nivell-1/Exercici-1/callback_hell.js
@@ -4,31 +4,27 @@ const { join } = require("path");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
-const reverseText = str =>
-  str
-  .split("")
-  .reverse()
-  .join("");
+const reverseText = str => str.split("").reverse().join("");
 
 // Read and reverse contents of text files in a directory
-fs.readdir(inbox, (error, files) => {
-  if (error) {
-    return console.log("Error: Folder inaccessible");
-  }
-
-  files.forEach(file => {
-    fs.readFile(join(inbox, file), "utf8", (error, data) => {
-      if (error) {
-        return console.log("Error: File error");
-      }
-
-      fs.writeFile(join(outbox, file), reverseText(data), error => {
-        if (error) {
-          return console.log("Error: File could not be saved!");
-        }
-
-        console.log(`${file} was successfully saved in the outbox!`);
-      });
-    });
-  });
-});
+// fs.readdir(inbox, (error, files) => {
+//   if (error) {
+//     return console.log("Error: Folder inaccessible");
+//   }
+//
+//   files.forEach(file => {
+//     fs.readFile(join(inbox, file), "utf8", (error, data) => {
+//       if (error) {
+//         return console.log("Error: File error");
+//       }
+//
+//       fs.writeFile(join(outbox, file), reverseText(data), error => {
+//         if (error) {
+//           return console.log("Error: File could not be saved!");
+//         }
+//
+//         console.log(`${file} was successfully saved in the outbox!`);
+//       });
+//     });
+//   });
+// });
