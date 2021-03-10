349e314cbd688b5e4ae178655913fab0d3298cab
DanMacGom <daniel.macias.gomez@gmail.com>
2021-03-10 21:06:57 +0100

    Added promises

--- 2.patterns/Nivell-1/Exercici-1/callback_hell.js
+++ 2.patterns/Nivell-1/Exercici-1/callback_hell.js
@@ -28,3 +28,42 @@ fs.readdir(inbox, (error, files) => {
     });
   });
 });
+
+const getFiles = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(inbox, (err, fl) => {
+      if (err) {
+        reject("Error: Folder inaccessible")
+      } else {
+        console.log(`Files are: ${fl}`)
+        resolve(fl);
+      }
+    });
+  });
+}
+
+const readFiles = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(join(inbox, file), "utf8", (err, data) => {
+      if (err) {
+        reject("Error: File error");
+      } else {
+        console.log(`${file} was read`)
+        resolve(data);
+      }
+    });
+
+  });
+}
+
+const writeFiles = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(join(outbox, file), reverseText(data), err => {
+      if (err) {
+        reject("Error: File could not be saved");
+      } else {
+        resolve(console.log(`${file} was successfully saved in the outbox!`));
+      }
+    });
+  });
+}
